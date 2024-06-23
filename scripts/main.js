b6db70c23f0d12244c69f1b00e387cffd5ec20c5
Ryan <13397120+ryanapil@users.noreply.github.com>
2024-06-23 07:53:13 +0100

    🚀

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+function memojiOver(element) {
+    element.setAttribute("src", "images/memoji-sunglasses.png");
+}
+function memojiOut(element) {
+    element.setAttribute("src", "images/memoji.png");
+}
